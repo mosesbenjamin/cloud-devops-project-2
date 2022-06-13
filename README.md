039a174d0d5a34462c3ecd7a2d7a33407544ecd3
mosesbenjamin <mavewreek@gmail.com>
2022-06-13 16:45:27 -0500

    Ammend code structure

---
+++ README.md
@@ -0,0 +1,2 @@
+##### Working Test
+  -  The high availability web app deployed using cloudformation is available here: `http://udagr-udagr-1mhmmwijfjv90-817150934.us-east-1.elb.amazonaws.com`
---
+++ deploy.sh
@@ -0,0 +1,1 @@
+aws cloudformation $1-stack --stack-name $2 --template-body file://$3  --parameters file://$4 --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-east-1 --profile=$5
